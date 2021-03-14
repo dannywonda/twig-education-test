a9799a4a4bfe38667d7c700e271ffd84b1849a6f
daniel <adesanya.daniel@yahoo.co.uk>
2021-03-14 16:14:24 +0000

    added a check for positive value

--- js/index.js
+++ js/index.js
@@ -8,41 +8,43 @@
 const array = [1, 2, 3, 4, 5];
 
 const groupArrayElements = (arrayValues, size) => {
+    let result = [], divide, remainder;
+
+    // check if values are empty
     if (arrayValues || size) {
-        let divide = arrayValues.length % size,
-            remainder = Math.floor(arrayValues.length / size),
-            result = [];
 
+        // check if size is positive
+        if (Math.sign(size) === 1) {
+            divide = arrayValues.length % size;
+            remainder = Math.floor(arrayValues.length / size);
 
-        // looping through the array
-        for (let i = 0; i < arrayValues.length; i += remainder) {
-            let end = remainder + i;
-            let add = false;
+            // looping through the array
+            for (let i = 0; i < arrayValues.length; i += remainder) {
+                let end = remainder + i;
+                let add = false;
 
-            // check the value of 0 in divide and start breaking the array
-            if (divide !== 0) {
-                end++;
-                divide--;
-                add = true;
-            }
+                // check the value of 0 in divide and start breaking the array
+                if (divide !== 0) {
+                    end++;
+                    divide--;
+                    add = true;
+                }
 
-            // add to the results
-            result.push(arrayValues.slice(i, end));
+                // add to the results
+                result.push(arrayValues.slice(i, end));
 
-            // more values then increase value of i
-            if (add) {
-                i++;
+                // more values then increase value of i
+                if (add) {
+                    i++;
+                }
             }
         }
-
-        return result;
-    } else {
-        return [];
     }
+    return result;
 }
 
 console.log(JSON.stringify(groupArrayElements(array, 2)));
 console.log(JSON.stringify(groupArrayElements(array, 3)));
 console.log(JSON.stringify(groupArrayElements(array, 4)));
 console.log(JSON.stringify(groupArrayElements(array, 5)));
-console.log(JSON.stringify(groupArrayElements(array)));
+console.log(JSON.stringify(groupArrayElements(array, -3)));
